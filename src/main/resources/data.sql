7b0ae706e91b02228788d83943129487db3b1b31
Rahul Choudhary <rahulchoudhary.bit@gmail.com>
2023-06-25 20:47:18 +0530

    Initial Commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,28 @@
+CREATE table person(
+id integer not null,
+name varchar (255) not null,
+location varchar (255) not null,
+birth_date timestamp,
+primary key (id)
+);
+
+INSERT INTO person( id,name,location,birth_date)
+values (1001, 'ramu', 'varanasi', CURRENT_TIMESTAMP());
+INSERT INTO person( id,name,location,birth_date)
+values (1002, 'shamu', 'varanasi', CURRENT_TIMESTAMP());
+INSERT INTO person( id,name,location,birth_date)
+values (1003, 'jamu', 'varanasi', CURRENT_TIMESTAMP());
+INSERT INTO person( id,name,location,birth_date)
+values (1004, 'tamu', 'varanasi', CURRENT_TIMESTAMP());
+INSERT INTO person( id,name,location,birth_date)
+values (1005, 'pamu', 'varanasi', CURRENT_TIMESTAMP());
+INSERT INTO person( id,name,location,birth_date)
+values (1006, 'indu', 'varanasi', CURRENT_TIMESTAMP());
+INSERT INTO person( id,name,location,birth_date)
+values (1007, 'bindu', 'varanasi', CURRENT_TIMESTAMP());
+INSERT INTO person( id,name,location,birth_date)
+values (1008, 'shindu', 'varanasi', CURRENT_TIMESTAMP());
+INSERT INTO person( id,name,location,birth_date)
+values (1009, 'dindu', 'varanasi', CURRENT_TIMESTAMP());
+INSERT INTO person( id,name,location,birth_date)
+values (1010, 'lindu', 'varanasi', CURRENT_TIMESTAMP());
